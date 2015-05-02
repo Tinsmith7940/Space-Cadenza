486819fbd27690239155b24bed1c34ea40757f95
srb043 <srb043@gmail.com>
2015-05-02 11:12:52 -0500

    Audio added

--- Trunk/Space-Cadenza/src/space/cadenza/characters/Alien.java
+++ Trunk/Space-Cadenza/src/space/cadenza/characters/Alien.java
@@ -9,7 +9,7 @@ public class Alien extends Character {
 	
 	public Alien(int health,int rateOfFire, String image, int xCoordinate, int yCoordinate, int speed)
 	{
-		super(xCoordinate, yCoordinate, speed, image, health,rateOfFire);
+		super(xCoordinate, yCoordinate, speed, image, health, rateOfFire);
 		
 	}
 	
